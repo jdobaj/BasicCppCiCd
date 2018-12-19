1f9e4efecec41e8ef431bd3460a5d8160d6790a3
jdobaj <juergen.dobaj@gmail.com>
2018-12-19 17:28:32 +0100

    add examples

--- docker/AH/docker-dev-env-run.sh
+++ docker/AH/docker-dev-env-run.sh
@@ -2,7 +2,7 @@ xhost +
 docker run -it \
 	--privileged \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-v ./QtExampleApp:/home/developer/QtExampleApp \
+	-v `pwd`/QtExampleApp:/home/developer/QtExampleApp \
 	-e DISPLAY=unix$DISPLAY \
 	--name qt-dev-env \
 	jdobaj/qt-dev-env:v1.0
